f1027c5f00d41ef4effaccc0c513970ea063fbc7
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2021-10-20 13:01:40 +0300

    Hochuchoto v1

---
+++ precache-manifest.29b2360d172582036115521654053b11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb9cc2d2c5393ec6261d16a9265d7d6",
+    "url": "/hochuchoto.github.io/index.html"
+  },
+  {
+    "revision": "21c69f549c19919a7fe0",
+    "url": "/hochuchoto.github.io/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "aa4710dc96bac5322bd6",
+    "url": "/hochuchoto.github.io/static/css/main.3f31a5dc.chunk.css"
+  },
+  {
+    "revision": "21c69f549c19919a7fe0",
+    "url": "/hochuchoto.github.io/static/js/2.6feed33a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hochuchoto.github.io/static/js/2.6feed33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4710dc96bac5322bd6",
+    "url": "/hochuchoto.github.io/static/js/main.59e0a419.chunk.js"
+  },
+  {
+    "revision": "d7915e265b1af04972c4",
+    "url": "/hochuchoto.github.io/static/js/runtime-main.422350a5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/hochuchoto.github.io/static/media/persik.4e1ec840.png"
+  }
+]);
